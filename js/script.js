9cc5d6eac72d61798d0673232b31a30267a3761d
Tamar <tamkopadze@gmail.com>
2020-02-26 18:14:31 +0400

    df

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-function myFunction(x) {
+function menuResponsive(x) {
   x.classList.toggle("change");
-}
+	document.getElementById("burgermenu").style.display = "block";
+
+	if (#burgermenu.style.display === "none") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+};
