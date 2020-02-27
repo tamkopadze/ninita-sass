f74b552139d01056d9fef3a94378888a4301a652
tamkopadze <49531010+tamkopadze@users.noreply.github.com>
2020-02-27 20:17:02 +0400

    lead banner

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-function menuResponsive(x) {
-  x.classList.toggle("change");
-	document.getElementById("burgermenu").style.display = "block";
+// function menuResponsive(x) {
+//   x.classList.toggle("change");
+// 	document.getElementById("burgermenu").style.display = "block";
 
-	if (#burgermenu.style.display === "none") {
-    menu.style.display = "block";
-  } else {
-    menu.style.display = "none";
-  }
-};
+// 	if (#burgermenu.style.display === "none") {
+//     menu.style.display = "block";
+//   } else {
+//     menu.style.display = "none";
+//   }
+// };
